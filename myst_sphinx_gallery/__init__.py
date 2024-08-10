3f466528cbfbd368ea2e69560f8836df73f388f6
fanchengyan <fanchy14@lzu.edu.cn>
2024-08-10 17:36:10 +0800

    update version to 0.1.1

--- myst_sphinx_gallery/__init__.py
+++ myst_sphinx_gallery/__init__.py
@@ -10,4 +10,4 @@ from .gallery import generate_gallery
 # dev versions should have "dev" in them, stable should not.
 # doc/conf.py makes use of this to set the version drop-down.
 # eg: "0.1.dev0", "0.1"
-__version__ = "0.1"
+__version__ = "0.1.1"
